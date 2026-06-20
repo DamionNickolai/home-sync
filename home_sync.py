feca122ca781ed14e3c20d344e44148cf86831c4
DamionNickolai <mr.jasongibson@yahoo.com>
2026-06-20 01:37:26 -0500

    I messed up version...fixed

--- home_sync.py
+++ home_sync.py
@@ -9,7 +9,7 @@ from database import get_active_tasks, get_completed_tasks, add_new_task, batch_
 from utils import calculate_next_version
 from supabase import create_client, Client
 
-APP_VERSION = "0.2.0"
+APP_VERSION = "0.1.0"
 GET_FIT_BASELINE_VERSION = "2.1.0"
 
 @st.cache_resource
